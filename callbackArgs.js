627c18ebc8e2dcf2b10f3bd0ab6c8852746fc12c
stevetice <tice.steve@gmail.com>
2017-06-29 12:51:13 -0700

    Replaced the for loop from callbackArgs.js with a forEach loop

--- callbackArgs.js
+++ callbackArgs.js
@@ -1,10 +1,16 @@
 // The second argument/parameter is expected to be a function
+// Replace for loop with for Each loop
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(arr[i], i);   // execute callback
-    }
-  }
+  arr.forEach(function findName(nameList, position) {
+    if (nameList === "Waldo") {
+      found(nameList, position);
+    };
+
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(arr[i], i);   // execute callback
+  //   }
+  });
 }
 
 function actionWhenFound(arrItem, index) {
